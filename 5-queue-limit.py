f0d8c74f753b8f00378ee74e569a542bb77e8e3d
Nichapat <6310301034@cdti.ac.th>
2023-08-23 15:19:17 +0700

    commit

--- 5-queue-limit.py
+++ 5-queue-limit.py
@@ -3,48 +3,51 @@ import asyncio
 import time
 
 # coroutine to generate work
-async def producer(queqe):
+
+
+async def producer(queue, id):
     print(f'{time.ctime()} Producer: Running')
     # generate work
     for i in range(10):
-        # generate work
+        # generate a value
         value = random()
         # block to simulate work
-        await asyncio.sleep(value)
-        # add to the queqe
-        await queqe.put(value)
-    print(f'{time.ctime()} Producer: Done')
+        await asyncio.sleep((id + 1)*0.1)
+        # add to the queue
+        await queue.put(value)
+    # send an all done signal
+    print(f'{time.ctime()} Producer {id}: Done')
 
-#coroutine to consume work
-async def consumer(queqe):
+
+async def consumer(queue):
     print(f'{time.ctime()} Consumer: Running')
     # consume work
     while True:
-        # get a unit for work
-        item = await queqe.get()
+        # get a unit of work
+        item = await queue.get()
         # report
-        print(f'{time.ctime()} >got {item}')
+        print(f'{time.ctime()} > got {item}')
         # block while processing
         if item:
             await asyncio.sleep(item)
-        # mark as completed
-        queqe.task_done()
+        # mark the task as done
+        queue.task_done()
     # all done
     print(f'{time.ctime()} Consumer: Done')
 
 # entry point coroutine
 async def main():
-    # create the shared queqe
+    # create a shared queue
     queue = asyncio.Queue(2)
-    # start the consumer
+    # start consumer
     _ = asyncio.create_task(consumer(queue))
-    # create many product
-    product = [producer(queue) for _ in range(5)]
+    # create many producers
+    producers = [producer(queue, i) for i in range(5)]
     # run and wait for the producers to finish
-    await asyncio.gather(*product)
-    # wait for the consumer to process all item
+    await asyncio.gather(*producers)
+    # run the producer and consumer
     await queue.join()
-    
+
 # start the asyncio program
 asyncio.run(main())
 
